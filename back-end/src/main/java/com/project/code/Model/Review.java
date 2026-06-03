0a56918981a7689933ab01665a6889f236f46da8
Richard <rj.n@live.com>
2026-06-03 15:07:23 +0100

    added document and getters and setters

--- back-end/src/main/java/com/project/code/Model/Review.java
+++ back-end/src/main/java/com/project/code/Model/Review.java
@@ -72,6 +72,45 @@ public class Review {
 
 // 10. Add Getters and Setters:
 //    - Add getter and setter methods for all fields (customerId, productId, storeId, rating, comment).
-  
 
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public Long getStoreId() {
+        return storeId;
+    }
+
+    public void setStoreId(Long storeId) {
+        this.storeId = storeId;
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
 }
