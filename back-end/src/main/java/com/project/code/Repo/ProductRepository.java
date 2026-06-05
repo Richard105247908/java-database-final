0ab5ddd7d8ae9c3582b681c91c62c0b7d9565c64
Richard <rj.n@live.com>
2026-06-05 12:27:05 +0100

    product repository

--- back-end/src/main/java/com/project/code/Repo/ProductRepository.java
+++ back-end/src/main/java/com/project/code/Repo/ProductRepository.java
@@ -1,7 +1,10 @@
 package com.project.code.Repo;
 
 
-public interface ProductRepository {
+import com.project.code.Model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
 // 1. Add the repository interface:
 //    - Extend JpaRepository<Product, Long> to inherit basic CRUD functionality.
 //    - This allows the repository to perform operations like save, delete, update, and find without having to implement these methods manually.
