7fb5742614276d505fe7a980f84ee936a17620f6
Richard <rj.n@live.com>
2026-06-09 13:28:23 +0100

    validate inventory and product

--- back-end/src/main/java/com/project/code/Service/ServiceClass.java
+++ back-end/src/main/java/com/project/code/Service/ServiceClass.java
@@ -1,17 +1,57 @@
 package com.project.code.Service;
 
 
+import com.project.code.Model.Inventory;
+import com.project.code.Model.Product;
+import com.project.code.Repo.InventoryRepository;
+import com.project.code.Repo.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ServiceClass {
-    
+
+    @Autowired
+    private final InventoryRepository inventoryRepository;
+    @Autowired
+    private final ProductRepository  productRepository;
+
+    public ServiceClass(InventoryRepository inventoryRepository, ProductRepository productRepository) {
+        this.inventoryRepository = inventoryRepository;
+        this.productRepository = productRepository;
+    }
+
+
 // 1. **validateInventory Method**:
 //    - Checks if an inventory record exists for a given product and store combination.
 //    - Parameters: `Inventory inventory`
 //    - Return Type: `boolean` (Returns `false` if inventory exists, otherwise `true`)
 
+   public Boolean validateInventory(Inventory inventory){
+      Inventory result=inventoryRepository.findByProductIdandStoreId(inventory.getProduct().getId(),inventory.getStore().getId());
+
+      if (result!=null){
+          return false;
+      }else{
+          return true;
+      }
+
+    }
+
 // 2. **validateProduct Method**:
 //    - Checks if a product exists by its name.
 //    - Parameters: `Product product`
 //    - Return Type: `boolean` (Returns `false` if a product with the same name exists, otherwise `true`)
+
+    public Boolean validateProduct(Product product){
+        Product exists=productRepository.findByName(product.getName());
+
+        if (product!=null){
+           return true;
+        }else{
+            return false;
+        }
+    }
 
 // 3. **ValidateProductId Method**:
 //    - Checks if a product exists by its ID.
