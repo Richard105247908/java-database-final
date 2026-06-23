2f4f76cb733eb9be86753c887315c6532cf77d26
Richard <rj.n@live.com>
2026-06-23 10:53:39 +0100

    handleJson completed

--- back-end/src/main/java/com/project/code/Controller/GlobalExceptionHandler.java
+++ back-end/src/main/java/com/project/code/Controller/GlobalExceptionHandler.java
@@ -1,7 +1,14 @@
 package com.project.code.Controller;
 
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class GlobalExceptionHandler {
@@ -17,5 +24,14 @@ public class GlobalExceptionHandler {
 //    - Use `@ResponseStatus(HttpStatus.BAD_REQUEST)` to specify that the response status will be **400 Bad Request** when this exception is thrown.
 //    - The method should return a `Map<String, Object>` with the following key:
 //        - **`message`**: The error message should indicate that the input provided is invalid. The value should be `"Invalid input: The data provided is not valid."`.
-    
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, Object> handleJsonParseException(){
+        Map<String, Object>map=new HashMap<>();
+        map.put("message","Invalid input: the data provided is not valid");
+        return map;
+
+    }
+
 }
